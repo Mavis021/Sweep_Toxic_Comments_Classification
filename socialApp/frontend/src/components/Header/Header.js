b035252a464bdcd44a0b2e5eda077bdeb85d9d59
Krish0110 <077bct038.krishala@pcampus.edu.np>
2024-12-02 22:44:40 +0545

    header added

--- socialApp/frontend/src/components/Header/Header.js
+++ socialApp/frontend/src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HeaderStyled,LogoImage} from './HeaderStyled';
+import {HeaderStyled,HeaderText,LogoImage} from './HeaderStyled';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import logo from '../../images/logo2.png'
@@ -8,6 +8,7 @@ const Header = () => {
   return (
     <HeaderStyled>
       <LogoImage src={logo} alt="flower"/>
+      <HeaderText>SWEEP-Toxic Comments Classifications</HeaderText>
       <FontAwesomeIcon icon={faBars} size="2x" />  {/* Bars icon */}
     </HeaderStyled>
   );
